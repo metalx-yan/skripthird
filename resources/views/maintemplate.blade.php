a2b50cc4798b4ac367857b2cedf8ad58cb6f451a
ALDIAN FIRMANSYAH <22020183@AD.BANKMEGA.LOCAL>
2022-08-17 21:12:33 +0700

    Finishs

--- resources/views/maintemplate.blade.php
+++ resources/views/maintemplate.blade.php
@@ -43,68 +43,16 @@
                         {{-- <p>PT. Anugrah Distributor Indonesia adalah sebuah perusahaan care car dan cat semprot yang berlokasi di Jalan Prabu Kian Santang, Tangerang. Perusahaan ini memberikan penyediaan solusi dan layanan perangkat keras serta perangkat lunak tingkat operator yang inovatif. Yang membantu bisnis sepenuhnya mewujudkan janji teknologi dan membantu memaksimalkan nilai teknologi yang Anda butuhkan.</p> --}}
                     </div>
                 </div>
-                <h1>Indoor</h1>
-                <div class="row">
-                    @foreach (App\Fasility::where('category_id', 1)->get() as $item)
-                        @if (is_null($item->booking))
-                            <div class="col-md-4">
-                                <div class="card" style="width: 18rem;">
-                                    <img src="{{ asset($item->image) }}" class="card-img-top"
-                                        style="display: block;
-                        margin: 0 auto;width:80%"
-                                        alt="...">
-                                    <div class="card-body">
-                                        <center>
-                                            <h5 class="card-title">{{ $item->name }}</h5>
-                                        </center>
-                                        <p class="card-text">Harga : {{ $item->price }}</p>
-                                        <p class="card-text">Deskripsi : {{ $item->desc }}</p>
-                                        <!-- Button trigger modal -->
-                                        <!-- Button trigger modal -->
-                                        <p>
-                                            <button class="btn btn-primary" type="button" data-toggle="collapse"
-                                                data-target="#collapseExample{{ $item->id }}" aria-expanded="false"
-                                                aria-controls="collapseExample">
-                                                Booking
-                                            </button>
-                                        </p>
-                                        <div class="collapse" id="collapseExample{{ $item->id }}">
-                                            <div class="card card-body">
-                                                <b>Transfer via rekening BCA</b>
-                                                <p><b><u>8797776690</u></b>
-                                                </p>
-                                                <p>Bukti pembayaran bisa langsung di upload :</p>
-                                                <form action="{{ route('bookings.store') }}" method="post"
-                                                    enctype="multipart/form-data">
-                                                    @csrf
-                                                    <input type="hidden" name="id" value="{{ $item->id }}">
-                                                    <label for="">Tanggal</label>
-                                                    {{-- <input id="date1"  placeholder="MM/DD/YYYY" data-input> --}}
-                                                    <input id="date1" size="60" name="tanggal" type="date"
-                                                        class="form-control" format="MM/DD/YYYY"
-                                                        placeholder="MM/DD/YYYY" required />
-                                                    <label for="">Bukti Pembayaran</label>
-                                                    <input type="file" name="image" class="form-control" required
-                                                        id="">
-                                                    <button type="submit"
-                                                        class="btn btn-primary btn-sm">Submit</button>
-                                                </form>
-                                            </div>
-                                        </div>
-
-                                    </div>
-                                </div>
-                                <br>
-                            </div>
-                        @else
-                            @if ($item->booking->status == '1')
-                            @else
+                @foreach (App\Category::all() as $cat)
+                    <h1>{{ ucwords($cat->name) }}</h1>
+                    <div class="row">
+                        @foreach ($cat->fasilities as $item)
+                            @if (is_null($item->booking))
                                 <div class="col-md-4">
                                     <div class="card" style="width: 18rem;">
                                         <img src="{{ asset($item->image) }}" class="card-img-top"
                                             style="display: block;
-                                margin: 0 auto;width:80%"
-                                            alt="...">
+        margin: 0 auto;width:80%" alt="...">
                                         <div class="card-body">
                                             <center>
                                                 <h5 class="card-title">{{ $item->name }}</h5>
@@ -149,116 +97,75 @@
                                     </div>
                                     <br>
                                 </div>
-                            @endif
-                        @endif
-                    @endforeach
-                </div>
-                <h1>Outdoor</h1>
-                <div class="row">
-                    @foreach (App\Fasility::where('category_id', 2)->get() as $item)
-                        {{-- <span>{{  }}</span> --}}
-                        @if (is_null($item->booking))
-                            <div class="col-md-4">
-                                <div class="card" style="width: 18rem;">
-                                    <img src="{{ asset($item->image) }}" class="card-img-top"
-                                        style="display: block;
-                        margin: 0 auto;width:80%"
-                                        alt="...">
-                                    <div class="card-body">
-                                        <center>
-                                            <h5 class="card-title">{{ $item->name }}</h5>
-                                        </center>
-                                        <p class="card-text">Harga : {{ $item->price }}</p>
-                                        <p class="card-text">Deskripsi : {{ $item->desc }}</p>
-                                        <!-- Button trigger modal -->
-                                        <p>
-                                            <button class="btn btn-primary" type="button" data-toggle="collapse"
-                                                data-target="#collapseExample{{ $item->id }}"
-                                                aria-expanded="false" aria-controls="collapseExample">
-                                                Booking
-                                            </button>
-                                        </p>
-                                        <div class="collapse" id="collapseExample{{ $item->id }}">
-                                            <div class="card card-body">
-                                                <form action="{{ route('bookings.store') }}" method="post"
-                                                    enctype="multipart/form-data">
-                                                    @csrf
-                                                    <input type="hidden" name="id"
-                                                        value="{{ $item->id }}">
-                                                    <label for="">Tanggal</label>
-                                                    {{-- <input id="date1"  placeholder="MM/DD/YYYY" data-input> --}}
-                                                    <input id="date1" size="60" name="tanggal"
-                                                        type="date" class="form-control" format="MM/DD/YYYY"
-                                                        placeholder="MM/DD/YYYY" required />
-                                                    <label for="">Bukti Pembayaran</label>
-                                                    <input type="file" name="image" class="form-control"
-                                                        required id="">
-                                                    <button type="submit"
-                                                        class="btn btn-primary btn-sm">Submit</button>
-                                                </form>
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                                <br>
-                            </div>
-                        @else
-                            @if ($item->booking->status == '1')
                             @else
-                                <div class="col-md-4">
-                                    <div class="card" style="width: 18rem;">
-                                        <img src="{{ asset($item->image) }}" class="card-img-top"
-                                            style="display: block;
-                            margin: 0 auto;width:80%"
-                                            alt="...">
-                                        <div class="card-body">
-                                            <center>
-                                                <h5 class="card-title">{{ $item->name }}</h5>
-                                            </center>
-                                            <p class="card-text">Harga : {{ $item->price }}</p>
-                                            <p class="card-text">Deskripsi : {{ $item->desc }}</p>
-                                            <!-- Button trigger modal -->
-                                            <p>
-                                                <button class="btn btn-primary" type="button" data-toggle="collapse"
-                                                    data-target="#collapseExample{{ $item->id }}"
-                                                    aria-expanded="false" aria-controls="collapseExample">
-                                                    Booking
-                                                </button>
-                                            </p>
-                                            <div class="collapse" id="collapseExample{{ $item->id }}">
-                                                <div class="card card-body">
-                                                    <form action="{{ route('bookings.store') }}" method="post"
-                                                        enctype="multipart/form-data">
-                                                        @csrf
-                                                        <input type="hidden" name="id"
-                                                            value="{{ $item->id }}">
-                                                        <label for="">Tanggal</label>
-                                                        {{-- <input id="date1"  placeholder="MM/DD/YYYY" data-input> --}}
-                                                        <input id="date1" size="60" name="tanggal"
-                                                            type="date" class="form-control" format="MM/DD/YYYY"
-                                                            placeholder="MM/DD/YYYY" required />
-                                                        <label for="">Bukti Pembayaran</label>
-                                                        <input type="file" name="image" class="form-control"
-                                                            required id="">
-                                                        <button type="submit"
-                                                            class="btn btn-primary btn-sm">Submit</button>
-                                                    </form>
+                                @if ($item->booking->status == '1')
+                                @else
+                                    <span>{{ is_null($item->booking) }}</span>
+                                    <div class="col-md-4">
+                                        <div class="card" style="width: 18rem;">
+                                            <img src="{{ asset($item->image) }}" class="card-img-top"
+                                                style="display: block;
+                margin: 0 auto;width:80%"
+                                                alt="...">
+                                            <div class="card-body">
+                                                <center>
+                                                    <h5 class="card-title">{{ $item->name }}</h5>
+                                                </center>
+                                                <p class="card-text">Harga : {{ $item->price }}</p>
+                                                <p class="card-text">Deskripsi : {{ $item->desc }}</p>
+                                                <!-- Button trigger modal -->
+                                                <!-- Button trigger modal -->
+                                                <p>
+                                                    <button class="btn btn-primary" type="button"
+                                                        data-toggle="collapse"
+                                                        data-target="#collapseExample{{ $item->id }}"
+                                                        aria-expanded="false" aria-controls="collapseExample">
+                                                        Booking
+                                                    </button>
+                                                </p>
+                                                <div class="collapse" id="collapseExample{{ $item->id }}">
+                                                    <div class="card card-body">
+                                                        <b>Transfer via rekening BCA</b>
+                                                        <p><b><u>8797776690</u></b>
+                                                        </p>
+                                                        <p>Bukti pembayaran bisa langsung di upload :</p>
+                                                        <form action="{{ route('bookings.store') }}" method="post"
+                                                            enctype="multipart/form-data">
+                                                            @csrf
+                                                            <input type="hidden" name="id"
+                                                                value="{{ $item->id }}">
+                                                            <label for="">Tanggal</label>
+                                                            {{-- <input id="date1"  placeholder="MM/DD/YYYY" data-input> --}}
+                                                            <input id="date1" size="60" name="tanggal"
+                                                                type="date" class="form-control"
+                                                                format="MM/DD/YYYY" placeholder="MM/DD/YYYY"
+                                                                required />
+                                                            <label for="">Bukti Pembayaran</label>
+                                                            <input type="file" name="image" class="form-control"
+                                                                required id="">
+                                                            <button type="submit"
+                                                                class="btn btn-primary btn-sm">Submit</button>
+                                                        </form>
+                                                    </div>
                                                 </div>
+
                                             </div>
                                         </div>
+                                        <br>
                                     </div>
-                                    <br>
-                                </div>
+                                @endif
                             @endif
-                        @endif
-                    @endforeach
-                </div>
+                        @endforeach
+                    </div>
+                @endforeach
+
             </div>
         </div>
+    </div>
 
 
-        {{-- @include('templates._footer') --}}
-        @include('templates._javascript')
+    {{-- @include('templates._footer') --}}
+    @include('templates._javascript')
 
 
     </div>
@@ -268,7 +175,7 @@
     const picker = document.getElementById('date1');
     picker.addEventListener('input', function(e) {
         var day = new Date(this.value).getUTCDay();
-        if ([5, 4, 3, 2, 1].includes(day)) {
+        if ([].includes(day)) {
             e.preventDefault();
             this.value = '';
             alert('Weekdayxs not allowed');
