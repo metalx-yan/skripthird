3239b89e2c9530a0f55c73b27a678aebf374e2f0
ALDIAN FIRMANSYAH <22020183@AD.BANKMEGA.LOCAL>
2022-08-22 10:14:51 +0700

    Done

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -30,7 +30,7 @@ class RegisterController extends Controller
      * @var string
      */
     // protected $redirectTo = RouteServiceProvider::HOME;
-    protected $redirectTo = '/customer';
+    protected $redirectTo = '/member';
 
     /**
      * Create a new controller instance.
